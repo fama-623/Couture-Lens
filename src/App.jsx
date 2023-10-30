dd78e040229ceb407d4f0f15658d71d86a3e7841
Farfama <farfama.hargaaya@gmail.com>
2023-10-30 21:29:36 +0900

    Add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
   )
 }
 
