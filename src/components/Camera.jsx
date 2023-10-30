f5f46fb798480a217376d43e775696c339558156
Farfama <farfama.hargaaya@gmail.com>
2023-10-30 22:14:19 +0900

    Fix image aspect ratio

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -26,10 +26,12 @@ function Camera() {
   }, []);
 
   return (
-    <div className="relative">
-      <video ref={videoRef} autoPlay playsInline className="w-full h-auto"></video>
-      <img src="/dress.png" alt="Dress Overlay" className="absolute top-0 left-0" />
-    </div>
+<div className="relative bg-white">
+  <div className="aspect-w-4 aspect-h-3 max-w-lg mx-auto">
+    <video ref={videoRef} autoPlay playsInline className="w-full h-full object-cover"></video>
+    <img src="/dress-2.png" alt="Dress Overlay" className="absolute top-0 left-0 w-full h-full object-cover" />
+  </div>
+</div>
   );
 }
 
