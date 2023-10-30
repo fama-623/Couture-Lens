b60509b04f77372c05ec5cf2b9fbe8101059f01b
Farfama <farfama.hargaaya@gmail.com>
2023-10-31 01:19:08 +0900

    Update save image
    - Resolution increase
    - Higher button placement

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -37,46 +37,36 @@ function Camera() {
     if (videoElement && canvasElement) {
       const context = canvasElement.getContext('2d');
   
-      // You can adjust this value for a different maximum resolution
-      const maxResolution = 1920;
-  
-      // Calculate the scaling factor based on maximum resolution
-      const scale = Math.min(maxResolution / videoElement.videoWidth, 1);
-      
-      // Set the canvas size based on video resolution and scaling factor
-      canvasElement.width = videoElement.videoWidth * scale;
-      canvasElement.height = videoElement.videoHeight * scale;
-  
-      // Draw a white rectangle to cover the entire canvas
-      context.fillStyle = 'white';
-      context.fillRect(0, 0, canvasElement.width, canvasElement.height);
-  
-      // Draw the video frame to the canvas while maintaining its aspect ratio
-      const videoAspectRatio = videoElement.videoWidth / videoElement.videoHeight;
-      let videoWidth = canvasElement.width;
-      let videoHeight = videoWidth / videoAspectRatio;
-      if (videoHeight > canvasElement.height) {
-        videoHeight = canvasElement.height;
-        videoWidth = videoHeight * videoAspectRatio;
-      }
-      const videoX = (canvasElement.width - videoWidth) / 2;
-      const videoY = (canvasElement.height - videoHeight) / 2;
-      context.drawImage(videoElement, videoX, videoY, videoWidth, videoHeight);
-  
-      // Draw the overlay image while maintaining its aspect ratio
+      // Load the overlay image
       const overlayImage = new Image();
       overlayImage.src = "/dress-2.png"; // Replace with the path to your overlay image
       overlayImage.onload = () => {
-        const overlayAspectRatio = overlayImage.width / overlayImage.height;
-        let overlayWidth = canvasElement.width;
-        let overlayHeight = overlayWidth / overlayAspectRatio;
-        if (overlayHeight > canvasElement.height) {
-          overlayHeight = canvasElement.height;
-          overlayWidth = overlayHeight * overlayAspectRatio;
+        // Set canvas size to overlay image size
+        canvasElement.width = overlayImage.width;
+        canvasElement.height = overlayImage.height;
+  
+        // Draw a white background
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, canvasElement.width, canvasElement.height);
+  
+        // Calculate the aspect ratio of the video
+        const videoAspectRatio = videoElement.videoWidth / videoElement.videoHeight;
+  
+        // Calculate dimensions to fit video within the overlay image
+        let videoWidth = canvasElement.width;
+        let videoHeight = videoWidth / videoAspectRatio;
+        if (videoHeight > canvasElement.height) {
+          videoHeight = canvasElement.height;
+          videoWidth = videoHeight * videoAspectRatio;
         }
-        const overlayX = (canvasElement.width - overlayWidth) / 2;
-        const overlayY = (canvasElement.height - overlayHeight) / 2;
-        context.drawImage(overlayImage, overlayX, overlayY, overlayWidth, overlayHeight);
+        const videoX = (canvasElement.width - videoWidth) / 2;
+        const videoY = (canvasElement.height - videoHeight) / 2;
+  
+        // Draw the video on the canvas
+        context.drawImage(videoElement, videoX, videoY, videoWidth, videoHeight);
+  
+        // Draw the overlay image on top
+        context.drawImage(overlayImage, 0, 0, canvasElement.width, canvasElement.height);
   
         // Create a data URL and trigger download
         const dataURL = canvasElement.toDataURL('image/png');
@@ -87,15 +77,18 @@ function Camera() {
       };
     }
   };
+  
+  
+  
 
   return (
     <div className="relative bg-white">
       <div className="aspect-w-4 aspect-h-3 max-w-lg mx-auto relative">
         <video ref={videoRef} autoPlay playsInline className="w-full h-full object-cover"></video>
         <img src="/dress-2.png" alt="Dress Overlay" className="absolute top-0 left-0 w-full h-full object-cover" />
-        <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+        <canvas  ref={canvasRef} style={{ display: 'none' }}></canvas>
       </div>
-      <div className="flex justify-center mt-24">
+      <div className="flex justify-center mt-4">
         <button onClick={handleSave} className="save-button bg-blue-500 text-white py-2 px-4 rounded">Save Image</button>
       </div>
     </div>
